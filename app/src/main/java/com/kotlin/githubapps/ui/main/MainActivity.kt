992708b19bb984a27eb687abb7802845aa1960de
Dian Tri Budianto <dyanzzz@gmail.com>
2021-07-28 14:32:22 +0700

    setup viewbinding

--- app/src/main/java/com/kotlin/githubapps/ui/main/MainActivity.kt
+++ app/src/main/java/com/kotlin/githubapps/ui/main/MainActivity.kt
@@ -2,11 +2,18 @@ package com.kotlin.githubapps.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kotlin.githubapps.R
+import com.kotlin.githubapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // implement viewbinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // implement viewbinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
