3f412e2d5c4da6f9a57c08e366af4b27e7b011eb
Dian Tri Budianto <dyanzzz@gmail.com>
2021-07-29 13:20:47 +0700

    Defined MainViewModel and UserAdapter to running Apps

--- app/src/main/java/com/kotlin/githubapps/ui/main/MainActivity.kt
+++ app/src/main/java/com/kotlin/githubapps/ui/main/MainActivity.kt
@@ -2,12 +2,18 @@ package com.kotlin.githubapps.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kotlin.githubapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     // implement viewbinding
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: UserAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +21,53 @@ class MainActivity : AppCompatActivity() {
         // implement viewbinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        adapter = UserAdapter()
+        adapter.notifyDataSetChanged()
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+
+        binding.apply {
+            rvUser.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvUser.setHasFixedSize(true)
+            rvUser.adapter = adapter
+
+            btnSearch.setOnClickListener {
+                searchUser()
+            }
+
+            etQuery.setOnKeyListener { _, keyCode, event ->
+                if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {// COMMA
+                    // COMMAND
+                        searchUser()
+                    return@setOnKeyListener true
+                }
+                return@setOnKeyListener false
+            }
+        }
+
+        viewModel.getSearchUser().observe(this, {
+            if (it != null){
+                adapter.setList(it)
+                showLoading(false)
+            }
+        })
+    }
+
+    private fun searchUser(){
+        binding.apply {
+            val query = etQuery.text.toString()
+            if (query.isEmpty()) return
+            showLoading(true)
+            println(query)
+            viewModel.setSearchUsers(query)
+        }
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
     }
 }
