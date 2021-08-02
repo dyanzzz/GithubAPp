3c51b58493555a077eae2d049b919d45f13ac5d9
Dian Tri Budianto <dyanzzz@gmail.com>
2021-08-02 13:27:21 +0700

    Implement lilbrary preference, setup class preference and create model  reminder

---
+++ app/src/main/java/com/kotlin/githubapps/data/model/Reminder.kt
@@ -0,0 +1,7 @@
+package com.kotlin.githubapps.data.model
+
+// create model reminder data class
+// untuk menampung/ menyimpan setPreference dari setting reminder
+data class Reminder(
+    var isReminded: Boolean = false
+)
