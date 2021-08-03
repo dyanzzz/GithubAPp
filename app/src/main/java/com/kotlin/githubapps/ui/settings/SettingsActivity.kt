7be2cec0c39a1fa70a5d316a35b723ff8fbe7b2a
Dian Tri Budianto <dyanzzz@gmail.com>
2021-08-03 08:55:25 +0700

    Implement SettingsActivity

--- app/src/main/java/com/kotlin/githubapps/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/kotlin/githubapps/ui/settings/SettingsActivity.kt
@@ -2,11 +2,51 @@ package com.kotlin.githubapps.ui.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kotlin.githubapps.R
+import com.kotlin.githubapps.data.model.Reminder
+import com.kotlin.githubapps.databinding.ActivitySettingsBinding
+import com.kotlin.githubapps.preference.ReminderPreference
+import com.kotlin.githubapps.receiver.AlarmReceiver
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+    private lateinit var reminder: Reminder
+    private lateinit var alarmReceiver: AlarmReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val reminderPreference = ReminderPreference(this)
+
+        // set kondisi untuk checkbox, ketika -- reminderPreference.getReminder().isReminded -- bernilai true/false
+        binding.switch1.isChecked = reminderPreference.getReminder().isReminded
+        /*if (reminderPreference.getReminder().isReminded) {
+            binding.switch1.isChecked = true
+        } else {
+            binding.switch1.isChecked = false
+        }*/
+
+        alarmReceiver = AlarmReceiver()
+
+        binding.switch1.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                saveReminder(true)
+                alarmReceiver.setRepeatingAlarm(this, "Repeating Alarm", "15:57", "Github Reminder")
+            } else {
+                saveReminder(true)
+                alarmReceiver.cancelAlarm(this)
+            }
+        }
+    }
+
+    private fun saveReminder(state: Boolean) {
+        val reminderPreference = ReminderPreference(this)
+        reminder = Reminder()
+
+        reminder.isReminded = state
+        reminderPreference.setReminder(reminder)
     }
 }
